32491daefc1a2a80deac016ab92b2dff81ee7d05
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-08-06 23:02:29 -0400

    Extract arg parsing to function

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate clap;
 use std::env;
 use std::error::Error;
 use std::io::Read;
+use std::path::PathBuf;
 use std::fs::OpenOptions;
 use open_read_later::read_later_list::ReadLaterList;
 use clap::{Arg, App, SubCommand, ArgMatches};
@@ -25,30 +26,7 @@ fn run() -> Result<i32, Box<Error>> {
     let mut default_list_file = home_dir;
     default_list_file.push(".read_later_list");
 
-    let args = App::new("readlater")
-                .version(env!("CARGO_PKG_VERSION"))
-                .author("Jeremy Dormitzer <jeremy.dormitzer@gmail.com>")
-                .about("Stores, queries, and manipulates read-later lists in the Open Read-Later specification format")
-                .arg(Arg::with_name("read_later_file")
-                     .short("f")
-                     .long("file")
-                     .value_name("FILE")
-                     .help("specifies the location of the list file")
-                     .takes_value(true)
-                     .default_value(default_list_file.to_str().unwrap()))
-                .subcommand(SubCommand::with_name("save")
-                            .about("saves a new link entry"))
-                .subcommand(SubCommand::with_name("edit")
-                            .about("edits a link entry"))
-                .subcommand(SubCommand::with_name("list")
-                            .about("lists link entries"))
-                .subcommand(SubCommand::with_name("search")
-                            .about("searches link entries by keyword"))
-                .subcommand(SubCommand::with_name("show")
-                            .about("shows a link entry"))
-                .subcommand(SubCommand::with_name("delete")
-                            .about("deletes a link entry"))
-                .get_matches();
+    let args = parse_args(&default_list_file);
 
     let list_file_path = args.value_of("read_later_file").unwrap();
     let mut list_file = OpenOptions::new()
@@ -70,6 +48,33 @@ fn run() -> Result<i32, Box<Error>> {
     }
 
     Ok(0)
+}
+
+fn parse_args<'a>(default_list_file: &'a PathBuf) -> ArgMatches<'a> {
+    App::new("readlater")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Jeremy Dormitzer <jeremy.dormitzer@gmail.com>")
+        .about("Stores, queries, and manipulates read-later lists in the Open Read-Later specification format")
+        .arg(Arg::with_name("read_later_file")
+             .short("f")
+             .long("file")
+             .value_name("FILE")
+             .help("specifies the location of the list file")
+             .takes_value(true)
+             .default_value(default_list_file.to_str().unwrap()))
+        .subcommand(SubCommand::with_name("save")
+                    .about("saves a new link entry"))
+        .subcommand(SubCommand::with_name("edit")
+                    .about("edits a link entry"))
+        .subcommand(SubCommand::with_name("list")
+                    .about("lists link entries"))
+        .subcommand(SubCommand::with_name("search")
+                    .about("searches link entries by keyword"))
+        .subcommand(SubCommand::with_name("show")
+                    .about("shows a link entry"))
+        .subcommand(SubCommand::with_name("delete")
+                    .about("deletes a link entry"))
+        .get_matches()
 }
 
 fn list(read_later_list: &ReadLaterList, list_args: &ArgMatches) {
